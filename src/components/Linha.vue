937109c5bca932554e0757a5ff805a1716a78e8c
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2024-04-24 22:25:16 -0400

    first commit

---
+++ src/components/Linha.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const props = defineProps(["pessoa"])
+</script>
+
+<template>
+    <tr>
+        <td>{{pessoa.nome}}</td>
+        <td>{{pessoa.cpf}}</td>
+        <td>{{pessoa.categoria}}</td>
+        <td></td>
+    </tr>
+</template>
